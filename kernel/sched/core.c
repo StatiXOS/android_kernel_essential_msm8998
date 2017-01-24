03907c7f0c20e817851aac81e80bcfad4f524056
Mathieu Poirier <mathieu.poirier@linaro.org>
2017-01-24 14:11:35 -0700

    sched/core: Fix &rd->cpudl memory leak
    While in the process of initialising a root domain, if function
    cpupri_init() fails the memory allocated in cpudl_init() is not
    reclaimed.
    
    Adding a new goto target to cleanup the previous initialistion of
    the root_domain's dl_bw structure reclaims said memory.
    
    Change-Id: I70bd3b8e7ec5f33d79168fd6b912495fd20da0d8
    Signed-off-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1485292295-21298-2-git-send-email-mathieu.poirier@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6926,7 +6926,7 @@ static int init_rootdomain(struct root_domain *rd)
 		goto free_rto_mask;
 
 	if (cpupri_init(&rd->cpupri) != 0)
-		goto free_rto_mask;
+		goto free_cpudl;
 
 	init_max_cpu_capacity(&rd->max_cpu_capacity);
 
@@ -6934,6 +6934,8 @@ static int init_rootdomain(struct root_domain *rd)
 
 	return 0;
 
+free_cpudl:
+	cpudl_cleanup(&rd->cpudl);
 free_rto_mask:
 	free_cpumask_var(rd->rto_mask);
 free_dlo_mask:
