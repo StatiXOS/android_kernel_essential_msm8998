b2fa38173b0700ed8ebfa8bbe269a64bb0f11591
celtare21 <celtare21@gmail.com>
2018-09-24 16:22:13 +0000

    zram: Fix mismerge
    Caf got the definition but not the actual commit the definition was made for, GG CAF
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -48,13 +48,6 @@ static const char *default_compressor = "lz4";
  */
 #define ALLOC_ERROR_LOG_RATE_MS 1000
 
-
-/*
- * We don't need to see memory allocation errors more than once every 1
- * second to know that a problem is occurring.
- */
-#define ALLOC_ERROR_LOG_RATE_MS 1000
-
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
 /*
